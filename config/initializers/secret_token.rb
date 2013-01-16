cc1b87a330f971cb4611f67eef4995c90519557c
Richard Lee <rlee@thoughtworks.com>
2013-01-15 19:34:22 -0600

    blank app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchoolTweets::Application.config.secret_token = 'f04dc88194859cb5fc5f0ed09e6cc82032a09cf22bfe8d63d773b2478ea49d91f2b985b0a08777eac0a97ae7992e231603d98caed6560bfee3c0dcb6af982eda'
